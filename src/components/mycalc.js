b834c702ff72479b1dac1259827338ca1bc5f664
pavel <nikitin.pavel.nik@yandex.ru>
2018-01-22 20:33:13 +0700

    calculator

--- src/components/mycalc.js
+++ src/components/mycalc.js
@@ -6,8 +6,11 @@ class MyCalc extends Component {
         this.state = {
             numRev: '',
             numVideo: '',
-            servises: '',
-            totalPrice: ''
+            servis1: 0,
+            servis2: 0,
+            servis3: 0,
+            servis4: 0,
+            servis5: 0
         }
 
         this.numRevChange = this
@@ -36,6 +39,15 @@ class MyCalc extends Component {
             this.setState({numVideo: ''})
     }
 
+    savecheckbox = (e) => {
+        var state = this.state
+        if (e.target.checked == true) 
+            state[e.target.name] = e.target.value
+        else 
+            state[e.target.name] = 0
+        this.setState(state);
+        console.log(this.state)
+    }
 
     render() {
         if (this.state.numRev !== '' && this.state.numRev > 0) {
@@ -51,50 +63,72 @@ class MyCalc extends Component {
         }
 
         var reviewsPrice = parseFloat(numRev) * 200 + parseFloat(numVideo) * 500;
-        var checkbox1 = this.refs.check1
-        
-        console.log(checkbox1)
-        var totalPrice = reviewsPrice;
+        var servisesPrice = parseFloat(this.state.servis1) + parseFloat(this.state.servis2) + parseFloat(this.state.servis3) + parseFloat(this.state.servis4) + parseFloat(this.state.servis5)
+        var totalPrice = reviewsPrice + servisesPrice;
 
         return (
-            <form>
-                <div className="form-group">
-                    <label>Количество отзывов</label>
-                    <input
-                        type="number"
-                        value={this.state.numRev}
-                        className="form-control inputone"
-                        id="reviews"
-                        placeholder="Введите количество отзывов"
-                        onChange={this.numRevChange}/>
+            <form className="row calculator">
+                <div className="col-xs-10 col-xs-push-1 ">
+                    <div className="form-group myformgroup col-xs-12 col-sm-6 text-center">
+                        <label>Количество отзывов</label>
+                        <input
+                            type="number"
+                            value={this.state.numRev}
+                            className="form-control myinput center-block"
+                            id="reviews"
+                            onChange={this.numRevChange}/>
+                    </div>
+                    <div className="form-group col-xs-12 col-sm-6 text-center">
+                        <label>Количество видеоотзывов</label>
+                        <input
+                            type="number"
+                            value={this.state.numVideo}
+                            className="form-control myinput center-block"
+                            id="videoreviews"
+                            onChange={this.numVideoChange}/>
+                    </div>
+                    <div className="form-group col-xs-12 ">
+                        <div className="checkboxgroup center-block">
+                            <div className="checkbox block-center">
+                                <label><input
+                                    type="checkbox"
+                                    value="1100"
+                                    name="servis1"
+                                    onChange={this.savecheckbox}/>Экспресс анализ оптимизации</label>
+                            </div>
+                            <div className="checkbox mycheckbox">
+                                <label><input
+                                    type="checkbox"
+                                    value="1200"
+                                    name="servis2"
+                                    onChange={this.savecheckbox}/>Экспресс анализ контекстной рекламы</label>
+                            </div>
+                            <div className="checkbox mycheckbox ">
+                                <label><input
+                                    type="checkbox"
+                                    value="1500"
+                                    name="servis3"
+                                    onChange={this.savecheckbox}/>Первичный анализ юзабилити</label>
+                            </div>
+                            <div className="checkbox mycheckbox ">
+                                <label><input
+                                    type="checkbox"
+                                    value="1400"
+                                    name="servis4"
+                                    onChange={this.savecheckbox}/>Экспресс анализ конкурентов</label>
+                            </div>
+                            <div className="checkbox mycheckbox ">
+                                <label><input
+                                    type="checkbox"
+                                    value="1600"
+                                    name="servis5"
+                                    onChange={this.savecheckbox}/>Передача веса сайта донора</label>
+                            </div>
+                        </div>
+                        <div className="totalprice">{totalPrice}
+                        </div>
+                        рублей</div>
                 </div>
-                <div className="form-group">
-                    <label>Количество видеоотзывов</label>
-                    <input
-                        type="number"
-                        value={this.state.numVideo}
-                        className="form-control inputone"
-                        id="videoreviews"
-                        placeholder="Введите количество видеоотзывов"
-                        onChange={this.numVideoChange}/>
-                    <div className="checkbox mycheckbox">
-                        <label><input type="checkbox" value="1100" ref="check1" />Экспресс анализ оптимизации</label>
-                    </div>
-                    <div className="checkbox mycheckbox">
-                        <label><input type="checkbox" value="1200" ref="check2"/>Экспресс анализ контекстной рекламы</label>
-                    </div>
-                    <div className="checkbox mycheckbox ">
-                        <label><input type="checkbox" value="1500" ref="check3"/>Первичный анализ юзабилити</label>
-                    </div>
-                    <div className="checkbox mycheckbox ">
-                        <label><input type="checkbox" value="1400" ref="check4"/>Экспресс анализ конкурентов</label>
-                    </div>
-                    <div className="checkbox mycheckbox ">
-                        <label><input type="checkbox" value="1600" ref="check5"/>Передача веса сайта донора на свой сайт</label>
-                    </div>
-                </div>
-                <div className="totalprice">{totalPrice}
-                    рублей</div>
             </form>
 
         );
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,70 +12,75 @@ import ContactForm from '../components/contactform'
 import MyModal from '../components/mymodal'
 import MyCalc from '../components/mycalc'
 
-
-
-
-
 class IndexPage extends React.Component {
-
 
   constructor(props) {
     super(props);
 
-    
   }
 
   componentDidMount() {
     const isBrowser = typeof window !== 'undefined';
-    const AOS = isBrowser ? require('aos') : undefined;
+    const AOS = isBrowser
+      ? require('aos')
+      : undefined;
     this.aos = AOS
-    this.aos.init()
-}
+    this
+      .aos
+      .init()
+  }
 
-componentDidUpdate() {
-    this.aos.refresh()
-}
-
- 
-
+  componentDidUpdate() {
+    this
+      .aos
+      .refresh()
+  }
 
   render() {
 
-
     return (
-      
+
       <div>
-      <div className="banner" id="home">
-      <div className="banner-background row text-center">
-          <div className="banner-girl"></div>
-          <h1 className="colorheader">
+        <div className="banner" id="home">
+          <div className="banner-background row text-center">
+            <div className="banner-girl"></div>
+            <h1 className="colorheader">
               <div className="banner-conversion">Рост конверсии до 20%</div>
               Результативное продвижение
-          </h1>
-          <h4>
-              СДЕЛАЕМ БЕСПЛАТНЫЙ 
-              <br/> АУДИТ САЙТА</h4>
-          <div className="banner-overlay"></div>
-      </div>
-  </div>
-        <div className="row whatyouget text-center" id="_1" >
+            </h1>
+            <h4>
+              СДЕЛАЕМ БЕСПЛАТНЫЙ
+              <br/>
+              АУДИТ САЙТА</h4>
+            <div className="banner-overlay"></div>
+          </div>
+        </div>
+        <div className="row whatyouget text-center" id="_1">
           <h1>ЧТО ВЫ ПОЛУЧИТЕ</h1>
           <div className="col-xs-12 ">
             <div className="slash center-block"></div>
           </div>
           <div className="container-fluid">
             <div className="row whatyouget-description">
-              <div  className="col-xs-5 whatyouget-image" data-aos="fade-right"  data-aos-easing="ease-in-sine" data-aos-duration="600">
-                <img src={advantage} alt="" className="img-responsive" />
+              <div
+                className="col-xs-5 whatyouget-image"
+                data-aos="fade-right"
+                data-aos-easing="ease-in-sine"
+                data-aos-duration="600">
+                <img src={advantage} alt="" className="img-responsive"/>
               </div>
               <div className="col-xs-7   whatyouget-list">
                 <h6>
-                  <div className="whatyouget-text center-block" data-aos="fade-left"  data-aos-easing="ease-in-sine" data-aos-duration="600">
-                  <p><br />➢&nbsp;ЭКСПРЕСС-АНАЛИЗ SEO ОПТИМИЗАЦИИ САЙТА;<br /></p>
-                  <p>➢&nbsp;ЭКСПРЕСС-АНАЛИЗ КОНТЕКСТНОЙ РЕКЛАМЫ;<br /></p>
-                  <p>➢&nbsp;ПЕРВИЧНЫЙ АНАЛИЗ ЮЗАБИЛИТИ;<br /></p>
-                  <p>➢ЭКСПРЕСС-АНАЛИЗ КОНКУРЕНТОВ;<br /></p>
-                  <p>➢&nbsp;ПЕРЕДАЧУ ВЕСА&nbsp;ОТ САЙТА ДОНОРА НА СВОЙ САЙТ;<br /></p>
+                  <div
+                    className="whatyouget-text center-block"
+                    data-aos="fade-left"
+                    data-aos-easing="ease-in-sine"
+                    data-aos-duration="600">
+                    <p><br/>➢&nbsp;ЭКСПРЕСС-АНАЛИЗ SEO ОПТИМИЗАЦИИ САЙТА;<br/></p>
+                    <p>➢&nbsp;ЭКСПРЕСС-АНАЛИЗ КОНТЕКСТНОЙ РЕКЛАМЫ;<br/></p>
+                    <p>➢&nbsp;ПЕРВИЧНЫЙ АНАЛИЗ ЮЗАБИЛИТИ;<br/></p>
+                    <p>➢ЭКСПРЕСС-АНАЛИЗ КОНКУРЕНТОВ;<br/></p>
+                    <p>➢&nbsp;ПЕРЕДАЧУ ВЕСА&nbsp;ОТ САЙТА ДОНОРА НА СВОЙ САЙТ;<br/></p>
                   </div>
                 </h6>
               </div>
@@ -89,12 +94,23 @@ componentDidUpdate() {
           </div>
           <div className="container-fluid">
             <div className="row prepare-description">
-              <div className="col-xs-6 prepare-image" data-aos="fade-right"  data-aos-easing="ease-in-sine" data-aos-duration="600">
-                <img src={phone} alt="" className="img-responsive" />
+              <div
+                className="col-xs-6 prepare-image"
+                data-aos="fade-right"
+                data-aos-easing="ease-in-sine"
+                data-aos-duration="600">
+                <img src={phone} alt="" className="img-responsive"/>
               </div>
               <div className="col-xs-6  prepare-description">
-                <div className="prepare-text" data-aos="fade-left"  data-aos-easing="ease-in-sine" data-aos-duration="600">
-                  <span>Наши отзывы готовит группа сотрудников с различным стилем речи и написания текста. Все тексты отзывов создаются индивидуально для каждого проекта с уникальностью не менее 95%. Именно такие тексты вызывают доверие других пользователей.</span>
+                <div
+                  className="prepare-text"
+                  data-aos="fade-left"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="600">
+                  <span>Наши отзывы готовит группа сотрудников с различным стилем речи и написания
+                    текста. Все тексты отзывов создаются индивидуально для каждого проекта с
+                    уникальностью не менее 95%. Именно такие тексты вызывают доверие других
+                    пользователей.</span>
                 </div>
               </div>
             </div>
@@ -107,7 +123,7 @@ componentDidUpdate() {
           </div>
           <div className="container-fluid">
             <div className="row">
-              <Carousel />
+              <Carousel/>
             </div>
           </div>
         </div>
@@ -119,23 +135,37 @@ componentDidUpdate() {
           </div>
           <div className="container-fluid">
             <div className="row result-description">
-              <div className="col-xs-2" data-aos="fade-down-right"  data-aos-easing="ease-in-sine" data-aos-duration="600">
-                <img src={resultimage} alt="image" className="img-responsive img-circle result-image" />
+              <div
+                className="col-xs-2"
+                data-aos="fade-down-right"
+                data-aos-easing="ease-in-sine"
+                data-aos-duration="600">
+                <img
+                  src={resultimage}
+                  alt="image"
+                  className="img-responsive img-circle result-image"/>
               </div>
-              <div className="col-xs-10 result-text" data-aos="fade-up-left"  data-aos-easing="ease-in-sine" data-aos-duration="600">
+              <div
+                className="col-xs-10 result-text"
+                data-aos="fade-up-left"
+                data-aos-easing="ease-in-sine"
+                data-aos-duration="600">
                 <span>
-                  <p>Раньше никто в нашей компании не знал, что есть люди, у которых можно заказать видео отзыв. Обходились
-                                своими силами. Мы тогда уже заметили, что наличие такого отзыва повышает интерес к сайту.</p>
-                  <p>Проблема была в том, что отзыва от клиента не допросишься. То у него времени нет, то забыл. А
-                                многие просто стесняются говорить на камеру. Или не хотят светиться в интернете. У нас висело
-                                2 ролика 2-летней давности. Смотрелось это грустно как-то.</p>
-                  Когда мы узнали об этой услуге, то решили заказать видео отзывы. Взяли 10 штук. Разместили на сайте. Конверсия сразу подросла.
-                            Мы за 2 года не могли такой добиться. Выросло среднее время нахождения посетителя на сайте. То
-                            есть люди реально смотрят эти видео. Заявки с контекстной рекламы стали обходиться нам на 30%
-                            дешевле. После этого заказали еще 30 фото отзывов с названием компании. Тоже на сайте выложили
-                            и в соц. сетях. Очень хорошо они работают. Это заметно и по числу заявок от клиентов, и по тем
-                            вопросам, которые они задают.
-                            <p>В целом мы очень довольны!</p>
+                  <p>Раньше никто в нашей компании не знал, что есть люди, у которых можно
+                    заказать видео отзыв. Обходились своими силами. Мы тогда уже заметили, что
+                    наличие такого отзыва повышает интерес к сайту.</p>
+                  <p>Проблема была в том, что отзыва от клиента не допросишься. То у него времени
+                    нет, то забыл. А многие просто стесняются говорить на камеру. Или не хотят
+                    светиться в интернете. У нас висело 2 ролика 2-летней давности. Смотрелось это
+                    грустно как-то.</p>
+                  Когда мы узнали об этой услуге, то решили заказать видео отзывы. Взяли 10 штук.
+                  Разместили на сайте. Конверсия сразу подросла. Мы за 2 года не могли такой
+                  добиться. Выросло среднее время нахождения посетителя на сайте. То есть люди
+                  реально смотрят эти видео. Заявки с контекстной рекламы стали обходиться нам на
+                  30% дешевле. После этого заказали еще 30 фото отзывов с названием компании. Тоже
+                  на сайте выложили и в соц. сетях. Очень хорошо они работают. Это заметно и по
+                  числу заявок от клиентов, и по тем вопросам, которые они задают.
+                  <p>В целом мы очень довольны!</p>
                 </span>
               </div>
             </div>
@@ -152,17 +182,23 @@ componentDidUpdate() {
                 <div className="row">
                   <div className="col-xs-12 col-sm-4">
                     <div className="row">
-                      <div className="beginner col-xs-8 col-xs-push-2 text-center" data-aos="flip-left"  data-aos-easing="ease-in-sine" data-aos-duration="600" >
+                      <div
+                        className="beginner col-xs-8 col-xs-push-2 text-center"
+                        data-aos="flip-left"
+                        data-aos-easing="ease-in-sine"
+                        data-aos-duration="600">
                         <h3>«НАЧАЛЬНЫЙ»</h3>
                         <h2>
-                          <span className="colorheader">10 000 </span> руб</h2>
+                          <span className="colorheader">10 000
+                          </span>
+                          руб</h2>
                         <h5>20 отзывов</h5>
                         <h5 className="unicip">Экспресс анализ SEO</h5>
                         <div className="region center-block">
                           <h5>---------</h5>
                         </div>
                         <div className="send center-block">
-                           <MyModal heading= 'Тариф начальный'/>
+                          <MyModal heading='Тариф начальный'/>
                         </div>
                         <h5 className="copyright"></h5>
                       </div>
@@ -170,17 +206,23 @@ componentDidUpdate() {
                   </div>
                   <div className="col-xs-12 col-sm-4">
                     <div className="row">
-                      <div className="standart col-xs-8 col-xs-push-2 text-center" data-aos="flip-left"  data-aos-easing="ease-in-sine" data-aos-duration="600" data-aos-delay="300">
+                      <div
+                        className="standart col-xs-8 col-xs-push-2 text-center"
+                        data-aos="flip-left"
+                        data-aos-easing="ease-in-sine"
+                        data-aos-duration="600"
+                        data-aos-delay="300">
                         <h3>«СТАНДАРТНЫЙ»</h3>
                         <h2>
-                          <span className="colorheader">17 000 </span>руб</h2>
+                          <span className="colorheader">17 000
+                          </span>руб</h2>
                         <h5>50 отзывов</h5>
                         <h5 className="unicip">Экспресс анализ SEO</h5>
                         <div className="region center-block">
                           <h5>Анализ контекстной рекламы</h5>
                         </div>
                         <div className="send center-block">
-                            <MyModal heading= 'Тариф стандартный'/>
+                          <MyModal heading='Тариф стандартный'/>
                         </div>
                         <h5 className="copyright"></h5>
                       </div>
@@ -188,17 +230,24 @@ componentDidUpdate() {
                   </div>
                   <div className="col-xs-12 col-sm-4">
                     <div className="row">
-                      <div className="premium col-xs-8 col-xs-push-2 text-center" data-aos="flip-left"  data-aos-easing="ease-in-sine" data-aos-duration="600" data-aos-delay="600">
+                      <div
+                        className="premium col-xs-8 col-xs-push-2 text-center"
+                        data-aos="flip-left"
+                        data-aos-easing="ease-in-sine"
+                        data-aos-duration="600"
+                        data-aos-delay="600">
                         <h3>«ПРЕМИУМ»</h3>
                         <h2>
-                          <span className="colorheader">30 000</span> руб</h2>
+                          <span className="colorheader">30 000</span>
+                          руб</h2>
                         <h5>100 отзывов</h5>
                         <h5 className="unicip">Все услуги</h5>
                         <div className="region center-block">
-                          <h5> + Бонусы!</h5>
+                          <h5>
+                            + Бонусы!</h5>
                         </div>
                         <div className="send center-block">
-                            <MyModal heading= 'Тариф премиум'/>
+                          <MyModal heading='Тариф премиум'/>
                         </div>
                         <h5 className="copyright"></h5>
                       </div>
@@ -209,18 +258,23 @@ componentDidUpdate() {
                   <h5 className="refinement">*окончательные цены зависят от Ваших конкретных требований</h5>
                 </div>
                 <div className="row">
-                  <div className="video col-xs-10 col-xs-push-1 col-sm-6 col-sm-push-3" data-aos="zoom-in"  data-aos-easing="ease-in-sine" data-aos-duration="600">
-                    <img src={newpng} alt="logo" className="img-responsive" />
+                  <div
+                    className="video col-xs-10 col-xs-push-1 col-sm-6 col-sm-push-3"
+                    data-aos="zoom-in"
+                    data-aos-easing="ease-in-sine"
+                    data-aos-duration="600">
+                    <img src={newpng} alt="logo" className="img-responsive"/>
                     <h3>10 ВИДЕО ОТЗЫВОВ</h3>
                     <h2>
-                      <span className="colorheader">5 000</span> руб</h2>
+                      <span className="colorheader">5 000</span>
+                      руб</h2>
                     <div className="video-description  center-block">
-                      <h5>В некоторых нишах - этот отличный инструмент. Он показывает, что у вас хороший опыт работы
-                                    и много довольных клиентов.</h5>
-                      
+                      <h5>В некоторых нишах - этот отличный инструмент. Он показывает, что у вас
+                        хороший опыт работы и много довольных клиентов.</h5>
+
                     </div>
                     <div className="center-block send">
-                        <MyModal heading= 'Заказать видеоотзывы'/>
+                      <MyModal heading='Заказать видеоотзывы'/>
                     </div>
                   </div>
                 </div>
@@ -229,36 +283,41 @@ componentDidUpdate() {
             </div>
           </div>
         </div>
-        <div className="row mycalc">
-        <MyCalc/>
+        <div className="row  calculation">
+          <h1 className="text-center">КАЛЬКУЛЯТОР</h1>
+          <div className="col-xs-12 ">
+            <div className="slash center-block"></div>
+          </div>
+          <div className="container-fluid">
+            <MyCalc/>
+          </div>
         </div>
         <div className="row contacts text-center" id="_6">
-            <div className="contacts-overlay">
-                <h1>Остались вопросы? Свяжитесь с нами!</h1>
-                <div className="contacts-form col-xs-10 col-xs-offset-1 col-sm-5 col-sm-offset-1">
-                    <ContactForm url= {'mailer.php'}/>
-                </div>
-                <div className="contacts-viber col-xs-12 col-sm-6">
-                    <div className="row contacts-phone text-right">
-                        <div className="col-xs-12 ">
-                            <h3><div className="phonenumber">+7 555 555 55 55</div>
-                                <img src={telefon} alt="telefon" className="telefon"/>
-                                <img src={whatsapp} alt="whatsapp" className="whatsapp"/>
-                                <img src={viber} alt="viber" className="viber"/>
-                            </h3>
-                            <h3>example@email.com
-                                <img src={gmail} alt="gmail" className="gmail"/>
-                            </h3>
-                        </div>
-                    </div>
-                </div>
+          <div className="contacts-overlay">
+            <h1>Остались вопросы? Свяжитесь с нами!</h1>
+            <div
+              className="contacts-form col-xs-10 col-xs-offset-1 col-sm-5 col-sm-offset-1">
+              <ContactForm url={'mailer.php'}/>
             </div>
+            <div className="contacts-viber col-xs-12 col-sm-6">
+              <div className="row contacts-phone text-right">
+                <div className="col-xs-12 ">
+                  <h3>
+                    <div className="phonenumber">+7 555 555 55 55</div>
+                    <img src={telefon} alt="telefon" className="telefon"/>
+                    <img src={whatsapp} alt="whatsapp" className="whatsapp"/>
+                    <img src={viber} alt="viber" className="viber"/>
+                  </h3>
+                  <h3>example@email.com
+                    <img src={gmail} alt="gmail" className="gmail"/>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-        
+      </div>
 
-      
-     
     )
   }
 
